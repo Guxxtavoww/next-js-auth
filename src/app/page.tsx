21c6835ff3b80070b1ad90346ec2645fa9738334
Guxxtavoww <gustavoaugustocar@hotmail.com>
2024-01-06 16:07:20 -0300

    Chore: REPO INIT

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import { Button } from '@/components/ui/button';
+
+export default function Page() {
+  return (
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6 text-center">
+        <h1 className={'text-6xl font-semibold text-white drop-shadow-md'}>
+          🔐 Auth
+        </h1>
+        <p className="text-white text-lg">Um serviço de autenticação simples</p>
+        <div>
+          {/* <LoginButton asChild> */}
+          <Button variant="secondary" size="lg">
+            Fazer login
+          </Button>
+          {/* </LoginButton> */}
+        </div>
+      </div>
+    </main>
+  );
+}
