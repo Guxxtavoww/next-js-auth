3d90a1ab22e71fd4a20f6260a7171a2bf006b867
Guxxtavoww <gustavoaugustocar@hotmail.com>
2024-01-10 13:45:34 -0300

    Chore: server actions init

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -2,6 +2,7 @@ import * as z from 'zod';
 
 export const envSchema = z.object({
   POSTGRES_DB_URL: z.string(),
+  POSTGRES_DIRECT_URL: z.string(),
 });
 
 export type EnvType = z.infer<typeof envSchema>;
