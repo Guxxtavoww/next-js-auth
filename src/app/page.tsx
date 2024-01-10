3e8fcf0ff14663154894b749de3426f430cb0220
Guxxtavoww <gustavoaugustocar@hotmail.com>
2024-01-10 12:37:09 -0300

    Chore: server actions init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { LoginButton } from '@/components/auth/login-button';
 
 export default function Page() {
   return (
-    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+    <main className="flex h-screen flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
       <div className="space-y-6 text-center">
         <h1 className={'text-6xl font-semibold text-white drop-shadow-md'}>
           🔐 Auth
